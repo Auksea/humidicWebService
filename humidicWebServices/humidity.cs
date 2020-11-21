85a89e0a4f2704c3857210244e099ebe6b48cc4a
Rania Zarka <rani0401@edu.easj.dk>
2020-11-21 09:19:20 +0100

    class created

---
+++ humidicWebServices/humidity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace humidicWebServices
+{
+    public class Humidity
+    {
+        public float Level { get; set; }
+        public DateTime Date { get; set; }
+
+
+        public Humidity(float level, DateTime date)
+        {
+
+            Level = level;
+            Date = date;
+
+        }
+    }
+}
